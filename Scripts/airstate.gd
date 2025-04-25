f5760c1f4ccc502ec6d06b8d16e4a3cac855a459
NicolasHelou <nhelou01@manhattan.edu>
2025-04-25 16:08:23 -0400

    continued work on animations

--- Scripts/airstate.gd
+++ Scripts/airstate.gd
@@ -4,13 +4,14 @@ class_name Airstate
 
 @export var die_state : State
 @export var ground_state : State
-
-var ground_animation = "run"
+@export var ground_animation: String = "run"
+@export var die_animation: String = "die"
 
 func state_process(_delta):
+	if !character.is_on_floor():
+		playback.travel(die_animation)
 	if character.is_on_floor():
 		next_state = ground_state
-		#playback.travel(ground_animation)
 
 
 func state_input(event : InputEvent):
--- Scripts/groundstate.gd
+++ Scripts/groundstate.gd
@@ -6,6 +6,7 @@ class_name GroundState
 @export var air_state : State
 @export var jump_velocity : float = -300.0
 @export var die_animation : String = "die"
+@export var ground_animation : String = "run"
 
 func state_input(event : InputEvent):
 	if (event.is_action_pressed("jump")):
@@ -17,10 +18,10 @@ func state_process(_delta):
 	if!character.is_on_floor():
 		next_state = air_state
 
+
 func jump():
 	character.velocity.y = -600.0
 	next_state = air_state
-	playback.travel(die_animation)
 
 func die():
 	next_state = die_state
